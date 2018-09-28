9716f2f0534683aedc0da6648c4a36281f3c5827
invisnet <classicpress@invis.net>
2018-09-28 11:48:10 +0000

    Core emoji plugin

---
+++ src/wp-content/plugins/__core__emoji/emoji.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package __core__emoji
+ */
+
+/*
+Plugin Name: ClassicPress Core: Emoji
+Description: Support for Emojis
+Version: 0.1
+License: GPLv2 or later 
+Provides: __CORE__EMOJI
+*/
+
+namespace ClassicPress\Core\Plugin\Emoji;
+
+define('__CORE__EMOJI', true);
+
